2a6d4f4ef4618fa9a99d68c2a01c540bb018d705
tony-molumby <tony-molumby@gmail.com>
2018-05-26 17:35:45 -0700

    updated readme

--- src/utils/user.js
+++ src/utils/user.js
@@ -1,7 +1,6 @@
-let Client = {};
 
 /* eslint-disable no-undef */
-Client.login = (query, cb) => {    
+export function login (query, cb){    
     fetch('api/search', { 
         headers: {
         'Accept': 'application/json',
@@ -14,22 +13,9 @@ Client.login = (query, cb) => {
         .then(cb)
   }
 
-  Client
 
-  
-// Client.checkStatus = (response) => {
-//     if (response.status >= 200 && response.status < 300) {
-//         return response;
-//     }
-//     const error = new Error(`HTTP Error ${response.statusText}`);
-//     error.status = response.statusText;
-//     error.response = response;
-//     console.log(error); // eslint-disable-line no-console
-//     throw error;
-// }
   
 function parseJSON(response) {
     return response.json();
 }
   
-  export default Client;
